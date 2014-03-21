b5e12d45ae378bc6acb92572f12eeff4fcef76fe
JianNL <yishupeixun2@gmail.com>
2014-03-21 21:58:57 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+#Author : yj 
+#Created Time: 2014年03月21日 星期五 21时58分11秒
+#File Name:test.py
+######################################################
+print 'test!'
